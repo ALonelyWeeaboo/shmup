2add80cf4fd650a4a30a6f72284be07c8a55c0df
ALonelyWeeaboo <ALonelyWeeaboo@gmail.com>
2014-10-28 10:13:51 -0700

    seaAdded
    hj

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,8 @@
-﻿#include <AIE.h>
-#include <iostream>
+﻿#include <iostream>
 #include <string>
-#include "player.h"
+#include <aie.h>
+
+//screen resolution
 const int iScreenWidth = 1040;
 const int iScreenHeight = 820;
 
@@ -10,16 +11,28 @@ unsigned int menu;
 
 unsigned int credits;
 
-	
+struct creditscreen
+{
+	unsigned int SpriteID = CreateSprite("./images/credits.png", 1040, 820, true);
+
+	float x = 2080;
+	float y = 1640;
+
+	void Draw()
+	{
+		DrawSprite(SpriteID);
+
+	}
+};
+
 struct ship
 {
-	unsigned int SpriteID = CreateSprite("./images/cannon.png", 64, 98, true);
+	unsigned int SpriteID = CreateSprite("./images/ship player.png", 64, 98, true);
 public:
 
 	float x = 300;
 	float y = 300;
-	float ShipWidth = 64;
-	float ShipHeight = 98;
+	
 	float ShipSpeedX = 0;
 	float ShipSpeedY = 0;
 
@@ -38,7 +51,24 @@ public:
 
 };
 
+struct bullets
+{
+	unsigned int SpriteID = CreateSprite("./images/bullet.png", 10, 32, false);
 
+	float x = 0;
+	float y = 0;
+	float bulletwidth = 10;
+	float bulletheight = 32;
+	float bulletspeedX = 0;
+	float bulletspeedY = 0;
+
+	void Move(float deltatime)
+	{
+		x += bulletspeedX * deltatime;
+			y += bulletspeedY * deltatime;
+
+	}
+};
 
 
 struct alienShips
@@ -49,8 +79,8 @@ struct alienShips
 	float y = 410;
 	float enemyspeedx = 0;
 	float enemyspeedy = 0;
-	float fEnemyX = iScreenWidth * 0.2f;
-	float fEnemyY = iScreenHeight * 0.7f;
+	float fEnemyX = 1040 * 0.2f;
+	float fEnemyY = 820 * 0.7f;
 	
 
 	void Draw()
@@ -60,8 +90,8 @@ struct alienShips
 
 	void Move(float deltatime)
 	{
-		x -= enemyspeedx *deltatime;
-		y += enemyspeedy *deltatime;
+		x -= enemyspeedx *deltatime / 600;
+		y += enemyspeedy *deltatime / 600;
 		MoveSprite(SpriteID, x, y);
 	}
 };
@@ -82,6 +112,9 @@ void UpdateCredits()
 	credits = CreateSprite("./images/credits.png", 1040, 820, true);
 	MoveSprite(credits, 520, 410);
 	DrawSprite(credits);
+
+	float x = 520;
+	float y = 410;
 }
 
 
@@ -95,143 +128,188 @@ enum GAMESTATES
 
 
 
-
 int main(int argc, char* argv[])
 {
 	bool IsGameRunning = true;
 	Initialise(iScreenWidth, iScreenHeight, false, "I FEEL ASLEEP");
-	GAMESTATES eCurrentState = eMAINMENU;
-	bool GamePause = false;
+	
+	
 	ship player1;
 	player1.x = iScreenHeight * .5;
 	player1.y = 150;
-
-	alienShips enemyship;
-
-	float fPlayerWidth = iScreenWidth * 0.2f;
-	float fPlayerHeight = iScreenHeight *.7f;
-
-	//game loop
-	do
-	{
-
-		float deltatime = GetDeltaTime() * 8;
-		SetBackgroundColour(SColour(0x00, 0x00, 0x00, 0xFF));
-		
-		switch (eCurrentState)
+	
+	
+	unsigned int alienShips[1];
+	float fEnemyX = iScreenWidth * 0.2f;
+	float fEnemyY = iScreenHeight * 0.7f;
+	
+	creditscreen credits;
+	
+	
+		GAMESTATES eCurrentState = eMAINMENU;
+	
+		do
 		{
-		case eMAINMENU:
 
-			UpdateMainMenu();
-			ClearScreen();
+			float deltatime = GetDeltaTime() * 8;
+			SetBackgroundColour(SColour(0x00, 0x00, 0x00, 0xFF));
 
 
-			if (IsKeyDown(GLFW_KEY_ENTER))
-			{
-				eCurrentState = eGAMEPLAY;
-			}
+			//Exit Game if statement
 			if (IsKeyDown(GLFW_KEY_ESCAPE))
 			{
-				IsGameRunning = false;
-
-			}
-			if (IsKeyDown(GLFW_KEY_F1))
-			{
-				eCurrentState = eCREDITS;
-			}
-			break;
-
-
-
-		case eGAMEPLAY:
-			player1.Draw();
-			player1.Move(deltatime);
-			ClearScreen();
-			//controls
-			if (IsKeyDown(GLFW_KEY_UP))
-			{
-				player1.y += deltatime * 45.f;
-				if (player1.y > -128.f)
 				{
-					player1.y > -128.0f;
-					player1.Move(player1.y);
+					IsGameRunning = false;
+
 				}
 			}
-
-			//going down
-			if (IsKeyDown(GLFW_KEY_DOWN))
+			switch (eCurrentState)
 			{
-				player1.y -= deltatime * 45.f;
-				if (player1.y < -128.f)
+			case eMAINMENU:
+
+				UpdateMainMenu();
+				ClearScreen();
+				if (IsKeyDown(GLFW_KEY_ENTER))
 				{
-					player1.y < -128.0f;
-					player1.Move(player1.y);
+					eCurrentState = eGAMEPLAY;
+
 				}
-			}
+
+				break;
+				switch (eCurrentState);
+
+						{
+
+					
+
+								case eGAMEPLAY:
+									CreateSprite("./images/sea.png", 1040, 820, true);
+									ClearScreen();
+									player1.Draw();
+									player1.Move(deltatime);
+									for (int i = 0; i < 5; ++i)
+										{
+											alienShips[1] = CreateSprite("./images/invaders/invaders_1_00.png", 64, 98, true);
+											MoveSprite(alienShips[1], fEnemyX, fEnemyY);
+											DrawSprite(alienShips[1]);
+											fEnemyX += 0.00012 * 1040, 820;
+											if (fEnemyX > 1040 * .8f)
+											{
+													fEnemyX = 1040 * .2f;
+													fEnemyY -= 0.08 * 820;
+											}
+					
+										}
+
+					//exit
+					if (IsKeyDown(GLFW_KEY_ESCAPE))
+					{
+						{
+							IsGameRunning = false;
+
+						}
+					}
 
 
-			//going left
-			if (IsKeyDown(GLFW_KEY_LEFT))
-			{
-				player1.x -= deltatime * 45.f;
-				if (player1.x < 128.f)
+					//controls
+					if (IsKeyDown(GLFW_KEY_UP))
+					{
+						player1.y += deltatime * 35.F;
+						if (player1.y > -128.f)
+						{
+							player1.y > -128.0f;
+							player1.Move(player1.y);
+						}
+					}
+
+					//going down
+					if (IsKeyDown(GLFW_KEY_DOWN))
+					{
+						player1.y -= deltatime * 35.f;
+						if (player1.y < -128.f)
+						{
+							player1.y < -128.0f;
+							player1.Move(player1.y);
+						}
+					}
+
+
+					//going left
+					if (IsKeyDown(GLFW_KEY_LEFT))
+					{
+						player1.x -= deltatime * 35.f;
+						if (player1.x < 128.f)
+						{
+							player1.x < 128.f;
+							player1.Move(player1.x);
+						}
+					}
+
+
+					//going right
+					if (IsKeyDown(GLFW_KEY_RIGHT))
+					{
+						player1.x += deltatime * 35.f;
+						if (player1.x < 128.f)
+						{
+							player1.x < 128.f;
+							player1.Move(player1.x);
+						}
+						
+					}
+					//Displays credits
+					if (IsKeyDown(GLFW_KEY_F1))
+					{
+						eCurrentState = eCREDITS;
+
+					}
+					//making it so there are no boundries, like Astroids or Super Mario Brothers 2.
+					//also, I made it so when the picture goes beyond the 1080 point
+					if (player1.x >= 1090)
+					{
+						player1.x = -60;
+					}
+					if (player1.x <= -80)//-60
+					{
+						player1.x = 1090;
+					}
+
+					if (player1.y >= 880)
+					{
+						player1.y = -40;
+					}
+
+					if (player1.y <= -80)//-40
+					{
+						player1.y = 840;
+					}
+					
+				}
+				break;
+				case eCREDITS:
+					ClearScreen();
+				
+				credits.Draw();
+				credits.x = 840;
+				credits.y = 820;
+				if (IsKeyDown(GLFW_KEY_BACKSPACE))
 				{
-					player1.x < 128.f;
-					player1.Move(player1.x);
+					eCurrentState = eGAMEPLAY;
 				}
-			}
+				
 
-			//going right
-			if (IsKeyDown(GLFW_KEY_RIGHT))
-			{
-				player1.x += deltatime * 45.f;
-				if (player1.x < 128.f)
-				{
-					player1.x < 128.f;
-					player1.Move(player1.x);
-				}
-			}
-			
-			//making it so there are no boundries, like Astroids or Super Mario Brothers 2.
-			//also, I made it so when the picture goes beyond the 1080 point
-			if (player1.x >= 1090)
-			{
-				player1.x = -60;
-			}
-			if (player1.x <= -80)//-60
-			{
-				player1.x = 1090;
-			}
+				break;
+				default:
+					break;
 
-			if (player1.y >= 880)
-			{
-				player1.y = -40;
 			}
-
-			if (player1.y <= -80)//-40
-			{
-				player1.y = 840;
-			}
-
 		}
-		
-		
 
-		//for loop
-	
-				//green line from space invaders, I might make use of it
-			DrawLine(0, 40, iScreenWidth, 40, SColour(0x00, 0xFC, 0x00, 0xFF));
-			
-			//Ends game,
-			if (IsKeyDown(GLFW_KEY_ESCAPE))
-			{
-				IsGameRunning = false;
+		//Ends game,
 
-			}
-			
-		}
-	
 		while (FrameworkUpdate() == false && IsGameRunning);
+
 		Shutdown();
 		return 0;
-}
+
+};
